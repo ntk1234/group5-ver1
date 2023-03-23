60e1a79a200fba6eeb89fb31938f3e04775b3151
ntk1234 <dickyntk@gmail.com>
2023-03-23 23:15:34 +0800

    ver1
    test

---
+++ Assets/Timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public float myTimer=0;
+
+	Text text;
+
+	void Start () {
+		text = GetComponent<Text>();
+	}
+
+	void Update () {
+		myTimer += Time.deltaTime;
+		text.text = (myTimer).ToString("'Time:'00");
+	}
+}
+
+
