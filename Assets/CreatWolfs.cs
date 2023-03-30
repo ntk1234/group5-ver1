d8570f7e55ffa6263258251a773659e66cc46310
unknown <a1@tyit35833.vtc.hk>
2023-03-30 13:24:50 +0800

    ver.2.4
    生成敵人

---
+++ Assets/CreatWolfs.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+/// <summary>
+/// 设计随机事件
+/// </summary>
+public class CreatWolfs : MonoBehaviour
+{
+    float CreatTime = 5f; //设计创造狼的时间
+    GameObject Wolfs; //申请一个狼的模块
+
+    void Update()
+    {
+        CreatTime -= Time.deltaTime;    //开始倒计时
+        if (CreatTime <= 0)    //如果倒计时为0 的时候
+        {
+            CreatTime = Random.Range(3, 10);     //随机3到9秒内
+            GameObject obj = (GameObject)Resources.Load("Prefabs/WolfNormal");    //加载预制体到内存
+            Wolfs = Instantiate<GameObject>(obj);    //实例化敌人
+            Wolfs.transform.position = new Vector3(Random.Range(408f, 77f), 21f, Random.Range(87f, 397f));    //随机生成狼的位置
+        }
+
+    }
+}
---
+++ Assets/RandomPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPoint : MonoBehaviour
+{
+    private Transform[] trf;//创建随机点位置数组
+    public GameObject go;//敌人预制体&#xff0c;自己拖拽
+    public int enemyCount=0;//敌人数量
+
+    void Start()
+    {
+        int count = this.transform.childCount;//获得随机点的数量
+        trf = new Transform[count];//实例化数组,为什么要实例化&#xff0c;因为之前只是创建了数组&#xff0c;但数组中的值为null
+        for (int i = count-1; i >= 0; i--)//将敌人的位置存入数组&#xff0c;为什么这样写可以看我另一篇文章&#34;Unity中Transform child out of bounds的问题&#34;
+        {
+            trf[i] = transform.GetChild(i).transform;
+        }
+    }
+    private float CreatTime = 1f;//第一次创建敌人的时间
+    void Update()
+    {
+        CreatTime -= Time.deltaTime;    //开始倒计时
+
+        if (CreatTime <= 0&& enemyCount<4) //倒计时为0并且敌人数量小于4
+        {
+            GameObject go2=Instantiate(go, trf[Random.Range(0,trf.Length)].position, Quaternion.identity);//创建敌人&#xff0c;并且把它赋给go2&#xff0c;方便后续管理
+            CreatTime = 3;//后续敌人每3秒出来一次
+            enemyCount ++;//敌人数量加1
+            Destroy(go2, 4f);//存在4秒后消失
+            enemyCount--;//敌人消失后&#xff0c;数量减1
+        }
+    }
+}
