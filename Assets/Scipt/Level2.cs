3656773daed98e804cd87ca443c0da02ec706f48
ntk1234 <dickyntk@gmail.com>
2023-06-01 23:12:14 +0800

    4.6.2
    fix

--- Assets/Scipt/Level2.cs
+++ Assets/Scipt/Level2.cs
@@ -13,8 +13,8 @@ public class Level2 : MonoBehaviour
 
     void Update()
     {
-        // 自動保存生命值，然後切換到 Level 2
-        if (Time.timeSinceLevelLoad >= 18f)
+       
+        if (Time.timeSinceLevelLoad >= 20f)
         {
          
         
