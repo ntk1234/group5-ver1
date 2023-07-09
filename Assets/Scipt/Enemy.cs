de674321f7c547af1c603f17e80ac9c7644b7dd3
ntk1234 <dickyntk@gmail.com>
2023-07-09 18:35:52 +0800

    4.8.9
    FIX 敵人跟蹤 增加關閉在TITLE

--- Assets/Scipt/Enemy.cs
+++ Assets/Scipt/Enemy.cs
@@ -8,20 +8,43 @@ public class Enemy : MonoBehaviour
     public int damage = 10;
     public float moveSpeed = 3f;
     public GameObject damageEffectPrefab;
-    public int scoreValue = 10; // 增加分数
+    public int scoreValue = 10;
 
-    private Transform target;
+    public float detectionRadius = 10f;
+    public Transform[] targets;
+    private Transform currentTarget;
 
     private void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        GameObject[] playerObjects = GameObject.FindGameObjectsWithTag("Player");
+        targets = new Transform[playerObjects.Length];
+        for (int i = 0; i < playerObjects.Length; i++)
+        {
+            targets[i] = playerObjects[i].transform;
+        }
     }
 
     private void Update()
     {
-        transform.LookAt(target);
-        transform.position += transform.forward * moveSpeed * Time.deltaTime;
-        moveSpeed = Mathf.Clamp(moveSpeed, 0f, 10f);
+        // Check the distance to all targets and select the closest one
+        float closestDistance = Mathf.Infinity;
+        for (int i = 0; i < targets.Length; i++)
+        {
+            float distance = Vector3.Distance(transform.position, targets[i].position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                currentTarget = targets[i];
+            }
+        }
+
+        // Move towards the current target
+        if (currentTarget != null && closestDistance < detectionRadius)
+        {
+            transform.LookAt(currentTarget);
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            moveSpeed = Mathf.Clamp(moveSpeed, 0f, 10f);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -30,7 +53,6 @@ public class Enemy : MonoBehaviour
         {
             CharController playerController = other.gameObject.GetComponent<CharController>();
             CharController1 playerController1 = other.gameObject.GetComponent<CharController1>();
-            ;
 
             if (playerController != null)
             {
@@ -47,18 +69,18 @@ public class Enemy : MonoBehaviour
 
     private void Die()
     {
-       CharController playerController = FindObjectOfType<CharController>();
+        CharController playerController = FindObjectOfType<CharController>();
         CharController1 playerController1 = FindObjectOfType<CharController1>();
-        
+
         if (playerController != null)
         {
-            playerController.AddScore(scoreValue); // 增加分数
+            playerController.AddScore(scoreValue);
         }
         if (playerController1 != null)
         {
-            playerController1.AddScore(scoreValue); // 增加分数
+            playerController1.AddScore(scoreValue);
         }
-        
+
         Destroy(gameObject);
     }
 
